89b00a40f15ecedbbf529d49db5c00f71a3a68c0
Maxim <maxim.truhonin@gmail.com>
2019-03-13 00:54:50 +0500

    Fixing Issue #1

--- CustomRPC/Properties/AssemblyInfo.cs
+++ CustomRPC/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
