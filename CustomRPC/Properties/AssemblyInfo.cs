0a25ac7cc300525707b13b33378d01cfbec85e53
Maxim <maxim.truhonin@gmail.com>
2021-08-30 00:41:35 +0500

    Updated languages

--- CustomRPC/Properties/AssemblyInfo.cs
+++ CustomRPC/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.11.5.0")]
+[assembly: AssemblyFileVersion("1.11.6.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
