33b72d4e5d4bc03672f0c7723b95ffdb3a00f2fa
Maxim <maxim.truhonin@gmail.com>
2022-05-14 00:47:58 +0500

    Updated version to 1.14.12

--- CustomRPC/Properties/AssemblyInfo.cs
+++ CustomRPC/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.14.11.0")]
+[assembly: AssemblyFileVersion("1.14.12.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
