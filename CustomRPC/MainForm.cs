eeec3405e3e9fd386eab85e3f5acd23977134e90
Max <maxim.truhonin@gmail.com>
2023-04-06 22:40:52 +0500

    Added ro as a wiki language

--- CustomRPC/MainForm.cs
+++ CustomRPC/MainForm.cs
@@ -154,7 +154,7 @@ namespace CustomRPC
         /// <summary>
         /// List of locales docs.customrp.xyz is translated to.
         /// </summary>
-        readonly List<string> translatedWikiLocales = new List<string> { "de", "es", "fi", "fr", "ko", "pl", "ru" };
+        readonly List<string> translatedWikiLocales = new List<string> { "de", "es", "fi", "fr", "ko", "pl", "ro", "ru" };
 
         /// <summary>
         /// Unicode character "No-Break Space" (" ").
