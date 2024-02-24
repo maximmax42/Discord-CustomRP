f3ad97193b10e1b282ba1118e15a7a560078481a
Max <maxim.truhonin@gmail.com>
2024-02-25 02:14:22 +0500

    Fixed a potential bug in the release script

--- CustomRPC/Build/Publish.ps1
+++ CustomRPC/Build/Publish.ps1
@@ -1,8 +1,8 @@
 #$ver = Read-Host "Enter version"
 $verRaw = (Get-Item ..\bin\Release\CustomRP.exe).VersionInfo
 $ver = "$($verRaw.FileMajorPart).$($verRaw.FileMinorPart)"
-if ($verRaw.FileBuildPart -ne 0) { $ver = "$ver.$($verRaw.FileBuildPart)" }
-if ($verRaw.FilePrivatePart -ne 0) { $ver = "$ver.$($verRaw.FilePrivatePart)" }
+if ($verRaw.FileBuildPart -ne 0 -or $verRaw.FilePrivatePart -ne 0) { $ver += ".$($verRaw.FileBuildPart)" }
+if ($verRaw.FilePrivatePart -ne 0) { $ver += ".$($verRaw.FilePrivatePart)" }
 
 xcopy ..\bin\Release CustomRP /e /i /s /y /exclude:exclude.txt
 C:\Program` Files` `(x86`)\Inno` Setup` 6\ISCC.exe /DMyAppVersion=$ver Installer.iss
