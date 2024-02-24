0b4d0e2c84705e81129feb37f9c581a281e74a49
Max <maxim.truhonin@gmail.com>
2024-02-25 01:18:11 +0500

    Put a proper path to InnoSetup's compiler

--- CustomRPC/Build/Publish.ps1
+++ CustomRPC/Build/Publish.ps1
@@ -4,12 +4,10 @@ $ver = "$($verRaw.FileMajorPart).$($verRaw.FileMinorPart)"
 if ($verRaw.FileBuildPart -ne 0) { $ver = "$ver.$($verRaw.FileBuildPart)" }
 if ($verRaw.FilePrivatePart -ne 0) { $ver = "$ver.$($verRaw.FilePrivatePart)" }
 
-C:\PROGRA~2\INNOSE~1\ISCC.exe /DMyAppVersion=$ver Installer.iss
+C:\Program` Files` `(x86`)\Inno` Setup` 6\ISCC.exe /DMyAppVersion=$ver Installer.iss
 
 xcopy ..\bin\Release CustomRP /e /i /s /y /exclude:exclude.txt
 echo "start CustomRP.exe --second-instance" > "CustomRP\Start Second Instance.bat"
 Compress-Archive -Path CustomRP,"Windows 7",README.txt -DestinationPath "Artifacts\CustomRP $ver.zip" -CompressionLevel Optimal
 #C:\PROGRA~1\WinRAR\WinRAR.exe a -afzip -r "CustomRP $ver.zip" CustomRP "Windows 7" README.txt | Out-Null
 Remove-Item CustomRP -Recurse
-
-cmd /c 'pause'
