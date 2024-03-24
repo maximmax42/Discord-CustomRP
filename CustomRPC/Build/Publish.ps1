8d2139f28b07980f7bd1b4521e2a7e032791eab6
Max <maxim.truhonin@gmail.com>
2024-03-24 15:34:24 +0500

    Set env variable with app version during build

--- CustomRPC/Build/Publish.ps1
+++ CustomRPC/Build/Publish.ps1
@@ -1,23 +1,23 @@
 $env:GIT_REDIRECT_STDERR = '2>&1'
 
 $verRaw = (Get-Item ..\bin\Release\CustomRP.exe).VersionInfo
-$ver = "$($verRaw.FileMajorPart).$($verRaw.FileMinorPart)"
-if ($verRaw.FileBuildPart -ne 0 -or $verRaw.FilePrivatePart -ne 0) { $ver += ".$($verRaw.FileBuildPart)" }
-if ($verRaw.FilePrivatePart -ne 0) { $ver += ".$($verRaw.FilePrivatePart)" }
+$env:CUSTOMRP_VER = "$($verRaw.FileMajorPart).$($verRaw.FileMinorPart)"
+if ($verRaw.FileBuildPart -ne 0 -or $verRaw.FilePrivatePart -ne 0) { $env:CUSTOMRP_VER += ".$($verRaw.FileBuildPart)" }
+if ($verRaw.FilePrivatePart -ne 0) { $env:CUSTOMRP_VER += ".$($verRaw.FilePrivatePart)" }
 
 xcopy ..\bin\Release CustomRP /e /i /s /y /exclude:exclude.txt
 xcopy License.txt CustomRP
 xcopy "Privacy Policy.txt" CustomRP
 git clone https://github.com/jrsoftware/issrc
-C:\Program` Files` `(x86`)\Inno` Setup` 6\ISCC.exe /DMyAppVersion=$ver Installer.iss
+C:\Program` Files` `(x86`)\Inno` Setup` 6\ISCC.exe /DMyAppVersion=$env:CUSTOMRP_VER Installer.iss
 
 echo "start CustomRP.exe --second-instance" > "CustomRP\Start Second Instance.bat"
-Compress-Archive -Path CustomRP,"Windows 7",README.txt -DestinationPath "Artifacts\CustomRP $ver.zip" -CompressionLevel Optimal
+Compress-Archive -Path CustomRP,"Windows 7",README.txt -DestinationPath "Artifacts\CustomRP $env:CUSTOMRP_VER.zip" -CompressionLevel Optimal
 
-$hashes = "CustomRP $ver.exe`n    $((Get-FileHash "Artifacts\CustomRP $ver.exe" -Algorithm SHA384).Hash)`nCustomRP $ver.zip`n    $((Get-FileHash "Artifacts\CustomRP $ver.zip" -Algorithm SHA384).Hash)"
+$hashes = "CustomRP $env:CUSTOMRP_VER.exe`n    $((Get-FileHash "Artifacts\CustomRP $env:CUSTOMRP_VER.exe" -Algorithm SHA384).Hash)`nCustomRP $env:CUSTOMRP_VER.zip`n    $((Get-FileHash "Artifacts\CustomRP $env:CUSTOMRP_VER.zip" -Algorithm SHA384).Hash)"
 
 "=== SHA256 Hashes ==="
 $hashes
 "====================="
 
-$hashes > "Artifacts\CustomRP Hashes $ver.txt"
+$hashes > "Artifacts\CustomRP Hashes $env:CUSTOMRP_VER.txt"
